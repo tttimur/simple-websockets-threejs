82d060e34670b0cba70dbc6fdeb956cdf8fe0c23
T <timur@56.digital>
2020-03-12 16:41:39 -0400

    init

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express')
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'))
+
+var connectedPeers = []
+
+io.on('connection', function(socket){
+  socket.on('init', x => {
+    socket.broadcast.emit('initPeers', {
+      connectedPeers
+    })
+  })
+
+
+  socket.on('userjoin', color => {
+    socket.color = color
+    connectedPeers.push(socket)
+
+    socket.broadcast.emit('userjoined', {
+      id: socket.id,
+      color: socket.color
+    })
+  })
+
+  socket.on('position', position => {
+    console.log(position, socket.id)
+    io.emit('positionReceived', {
+      position, id: socket.id
+    })
+  })
+
+  socket.on('disconnect', function () {
+    connectedPeers = connectedPeers.filter(el => el.id !== socket.id)
+
+    socket.broadcast.emit('userleft', {
+      id: socket.id
+    })
+  })
+})
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
